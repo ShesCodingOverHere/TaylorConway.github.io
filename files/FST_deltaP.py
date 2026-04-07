a69c0ec7cfee28cf7721740d2a2aaa3fb594c8fd
ShesCodingOverHere <105224521+ShesCodingOverHere@users.noreply.github.com>
2026-04-07 11:52:37 -0500

    Update FST_deltaP.py

--- files/FST_deltaP.py
+++ files/FST_deltaP.py
@@ -1,67 +1,147 @@
+---
+title: "Allele Frequency Comparison and FST Calculation"
+---
+
+# Allele Frequency Comparison and FST Calculation
+
+This script compares allele frequencies between two populations and calculates both ΔP (allele frequency difference) and FST for each genomic region. It takes precomputed allele frequency files as input, parses genomic coordinates and counts, and outputs summary statistics for population differentiation.
+
+The workflow reads two input files representing separate populations, computes allele frequencies, and then calculates ΔP and FST for each shared genomic interval.
+
+---
+
+## Script
+
+```python
 from __future__ import division
 import math
 
-# read in files for p1 and p2
-InFile1=open("/nobackup/rogers_research/Taylor/OnceMoreWithFeeling/WithZerosAlleleFreqsIslandDYAK.out",'r')
-InFile2=open("/nobackup/rogers_research/Taylor/OnceMoreWithFeeling/WithZerosAlleleFreqsMainlandDYAK.out",'r')
-deltaPout=open("/nobackup/rogers_research/Taylor/OnceMoreWithFeeling/DYAKDeltaPWithZeros.out",'w')
+# =========================
+# Input / Output (generic paths)
+# =========================
+InFile1 = open("data/input/population1_allele_freqs.out", 'r')
+InFile2 = open("data/input/population2_allele_freqs.out", 'r')
+OutFile = open("results/output/deltaP_fst_results.tsv", 'w')
 
-p1Dict={}
-p2Dict={}
+p1Dict = {}
+p2Dict = {}
 
+# =========================
+# Step 1: Read allele frequency files
+# =========================
 def readDict(Infile, mydict):
-    for i in Infile:
-        A=i.split()
-        span=A[0].split('.')
-        chromosome=span[0]
-        start=int(span[1])
-        stop=int(span[2])
-        B=A[1].split('/')
-        num=int(B[0])
-        denom=int(B[1])
-        mykey=A[0]
-        if float(B[1])==0:
-            p=0
-        else:
-            p=float(B[0])/float(B[1])
-        mydict[mykey]=[chromosome,start,stop,num,denom,p]
-readDict(InFile1,p1Dict)
-readDict(InFile2,p2Dict)
 
+    for line in Infile:
+        A = line.split()
+
+        # Parse genomic coordinates
+        span = A[0].split('.')
+        chromosome = span[0]
+        start = int(span[1])
+        stop = int(span[2])
+
+        # Parse counts
+        B = A[1].split('/')
+        num = int(B[0])
+        denom = int(B[1])
+
+        key = A[0]
+
+        # Calculate allele frequency
+        if denom == 0:
+            p = 0
+        else:
+            p = float(num) / float(denom)
+
+        mydict[key] = [chromosome, start, stop, num, denom, p]
+
+
+readDict(InFile1, p1Dict)
+readDict(InFile2, p2Dict)
+
+
+# =========================
+# Step 2: Compute ΔP and FST
+# =========================
 for key in p1Dict:
-    chrom=p1Dict[key][0]
-    start=p1Dict[key][1]
-    stop=p1Dict[key][2]
-    num1=p1Dict[key][3]
-    denom1=p1Dict[key][4]
-    num2=p2Dict[key][3]
-    denom2=p2Dict[key][4]
-    freq1=p1Dict[key][5]
-    freq2=p2Dict[key][5]
 
-    #midpoint
-    mutStart=p1Dict[key][1]
-    mutStop=p1Dict[key][2]
-    mutMidpoint = str(math.trunc((int(mutStart) + int(mutStop)) / 2))
-    #FST
-    if (denom1+denom2)!=0:
-        p_bar=(num1+num2)/(denom1+denom2)
-        q_bar=1-p_bar
-        c_1=denom1/(denom1+denom2)
-        c_2=denom2/(denom1+denom2)
-        p_1=freq1
-        p_2=freq2
-        q_1=1-p_1
-        q_2=1-p_2
-        totalnum=num1+num2
-        totaldenom=denom1+denom2
-        totalfreq=totalnum/totaldenom
+    chrom = p1Dict[key][0]
+    start = p1Dict[key][1]
+    stop = p1Dict[key][2]
 
-        if (p_bar*q_bar)==0:
-            fst=0
+    num1 = p1Dict[key][3]
+    denom1 = p1Dict[key][4]
+    freq1 = p1Dict[key][5]
+
+    num2 = p2Dict[key][3]
+    denom2 = p2Dict[key][4]
+    freq2 = p2Dict[key][5]
+
+    # Midpoint of region
+    midpoint = math.trunc((start + stop) / 2)
+
+    if (denom1 + denom2) != 0:
+
+        # Combined allele frequency
+        p_bar = (num1 + num2) / (denom1 + denom2)
+        q_bar = 1 - p_bar
+
+        # Population weights
+        c1 = denom1 / (denom1 + denom2)
+        c2 = denom2 / (denom1 + denom2)
+
+        p1 = freq1
+        p2 = freq2
+        q1 = 1 - p1
+        q2 = 1 - p2
+
+        total_num = num1 + num2
+        total_denom = denom1 + denom2
+        total_freq = total_num / total_denom
+
+        # FST calculation
+        if (p_bar * q_bar) == 0:
+            fst = 0
         else:
-            fst=((p_bar*q_bar)-((p_1*q_1*c_1)+(p_2*q_2*c_2)))/(p_bar*q_bar)
+            fst = ((p_bar * q_bar) - ((p1 * q1 * c1) + (p2 * q2 * c2))) / (p_bar * q_bar)
 
         # Delta P
-        deltaP=p1Dict[key][5]-p2Dict[key][5]
-        deltaPout.write(chrom+"\t"+str(start)+"\t"+str(stop)+"\t"+str(mutMidpoint)+"\t"+str(num1)+"\t"+str(denom1)+"\t"+str(freq1)+"\t"+str(num2)+"\t"+str(denom2)+"\t"+str(freq2)+"\t"+str(deltaP)+"\t"+str(fst)+"\t"+str(totalfreq)+"\n")
+        deltaP = freq1 - freq2
+
+        # Output
+        OutFile.write(
+            chrom + "\t" +
+            str(start) + "\t" +
+            str(stop) + "\t" +
+            str(midpoint) + "\t" +
+            str(num1) + "\t" +
+            str(denom1) + "\t" +
+            str(freq1) + "\t" +
+            str(num2) + "\t" +
+            str(denom2) + "\t" +
+            str(freq2) + "\t" +
+            str(deltaP) + "\t" +
+            str(fst) + "\t" +
+            str(total_freq) + "\n"
+        )
+```
+
+---
+
+## Output
+
+The output file contains tab-separated values:
+
+```
+chrom  start  stop  midpoint  num1  denom1  freq1  num2  denom2  freq2  deltaP  fst  total_freq
+```
+
+---
+
+## Notes
+
+- ΔP represents the difference in allele frequency between two populations  
+- FST quantifies population differentiation at each genomic region  
+- Regions with zero total coverage are excluded from calculations  
+- Input files must share the same genomic coordinate keys  
+- Designed for downstream population genetics analyses  
